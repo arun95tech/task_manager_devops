057fff32a8cce8469ea7bc63799eff9fc3c57cf8
muhammadomer4812 <muhammadomer4812@gmail.com>
2026-04-24 18:48:35 +0100

    feat:create basic task service helth apt

---
+++ task_service/tasks/views.py
@@ -0,0 +1,10 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def health_check(request):
+    return Response({
+        "service": "task_service",
+        "status": "running"
+    })
